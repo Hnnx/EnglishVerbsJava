1366a0b539a69733844b75a4ea5248fc894773eb
HNNX <nejc.stibilj01@gmail.com>
2021-01-04 15:38:15 +0100

    Managing db file

--- EnglishVerbsJava/source/DB/SqliteConnect.java
+++ EnglishVerbsJava/source/DB/SqliteConnect.java
@@ -44,7 +44,7 @@ public class SqliteConnect {
 			
 			// Premakni dbFile v folder db, ki bi mogu bit kreiran ob zagonu programa
 
-			String dbFile = path + "\\bin\\DB\\vilka.db";
+			String dbFile = path + "\\vilka.db";
 			String destination = path+"\\db\\vilka.db";
 			File file1 = new File(dbFile);
 
@@ -86,6 +86,7 @@ public class SqliteConnect {
 			kreirajTabRoles();
 			kreirajTabhelperTable();
 			kreirajTabeloUsers2();
+			populateUsers();
 
 		} catch (SQLException e) {
 			JOptionPane.showMessageDialog(null, "Prišlo je do napake pri povezovanju z bazo podatkov" + e.toString(),
@@ -96,10 +97,16 @@ public class SqliteConnect {
 
 	private static void kreirajTabGlagol() throws SQLException {
 
-		query = "CREATE TABLE IF NOT EXISTS glagoli (\n" + "	id integer NOT NULL,\n" + "	prevod TEXT,\n"
-				+ "	verb TEXT,\n" + "	pastSimple TEXT,\n" + "	pastParticiple TEXT,\n"
-				+ " PRIMARY KEY(\"id\" AUTOINCREMENT));";
 
+		query ="CREATE TABLE \"glagoli\" (\r\n" + 
+				"	\"id\"	INTEGER NOT NULL,\r\n" + 
+				"	\"prevod\"	TEXT,\r\n" + 
+				"	\"verb\"	TEXT,\r\n" + 
+				"	\"pastSimple\"	TEXT,\r\n" + 
+				"	\"pastParticiple\"	TEXT,\r\n" + 
+				"	PRIMARY KEY(\"id\" AUTOINCREMENT)\r\n" + 
+				")";
+		
 		Statement stmt = conn.createStatement();
 		stmt.execute(query);
 
@@ -107,34 +114,71 @@ public class SqliteConnect {
 
 	private static void kreirajTabRoles() throws SQLException {
 
-		query = "CREATE TABLE IF NOT EXISTS roles (\n" + "	id integer NOT NULL,\n" + "	role TEXT,\n"
-				+ " PRIMARY KEY(\"id\" AUTOINCREMENT));";
-
+		query = "CREATE TABLE \"roles\" (\r\n" + 
+				"	\"id\"	INTEGER NOT NULL,\r\n" + 
+				"	\"role\"	TEXT,\r\n" + 
+				"	PRIMARY KEY(\"id\" AUTOINCREMENT)\r\n" + 
+				")";
+		
+		
 		Statement stmt = conn.createStatement();
 		stmt.execute(query);
 
 	}
 
 	private static void kreirajTabhelperTable() throws SQLException {
-		query = "CREATE TABLE IF NOT EXISTS helperTable (\n" + "	id integer NOT NULL,\n" + "	ucenec TEXT,\n"
-				+ "	glagol TEXT,\n" + "	glagol TEXT,\n"
-				+ "	FOREIGN KEY(\"ucenec\") REFERENCES \"users2\"(\"id\") ON DELETE CASCADE,\n"
-				+ " FOREIGN KEY(\"glagol\") REFERENCES \"glagoli\"(\"id\"),\n" + " PRIMARY KEY(\"id\" AUTOINCREMENT));";
-
+		query = "CREATE TABLE \"helperTable\" (\r\n" + 
+				"	\"id\"	INTEGER,\r\n" + 
+				"	\"ucenec\"	TEXT,\r\n" + 
+				"	\"glagol\"	TEXT,\r\n" + 
+				"	FOREIGN KEY(\"ucenec\") REFERENCES \"users2\"(\"id\") ON DELETE CASCADE,\r\n" + 
+				"	FOREIGN KEY(\"glagol\") REFERENCES \"glagoli\"(\"id\"),\r\n" + 
+				"	PRIMARY KEY(\"id\" AUTOINCREMENT)\r\n" + 
+				")";
+		
 		Statement stmt = conn.createStatement();
 		stmt.execute(query);
 
 	}
 
 	private static void kreirajTabeloUsers2() throws SQLException {
-		query = "CREATE TABLE IF NOT EXISTS users2 (\n" + "	id integer NOT NULL,\n" + "	username TEXT,\n"
-				+ "	password TEXT,\n" + "	sequence TEXT,\n" + "	role INTEGER,\n"
-				+ " PRIMARY KEY(\"id\" AUTOINCREMENT),\n"
-				+ " FOREIGN KEY(\"role\") REFERENCES \"roles\"(\"id\") ON DELETE CASCADE);";
 
+		query = "CREATE TABLE \"users2\" (\r\n" + 
+				"	\"id\"	INTEGER NOT NULL,\r\n" + 
+				"	\"username\"	TEXT,\r\n" + 
+				"	\"password\"	TEXT,\r\n" + 
+				"	\"sequence\"	TEXT,\r\n" + 
+				"	\"role\"	INTEGER,\r\n" + 
+				"	FOREIGN KEY(\"role\") REFERENCES \"roles\"(\"id\") ON DELETE CASCADE,\r\n" + 
+				"	PRIMARY KEY(\"id\" AUTOINCREMENT)\r\n" + 
+				")";
+		
+		
 		Statement stmt = conn.createStatement();
 		stmt.execute(query);
 
+	}
+	
+	private static void populateUsers() {
+		
+		try {
+			query = "INSERT INTO users2 (username, password, sequence, role) VALUES (?, ?, ?, ?)";
+			pSTMT = conn.prepareStatement(query);	
+			
+			pSTMT.setString(1, "admin");
+			pSTMT.setString(2, "21232f297a57a5a743894a0e4a801fc3");
+			pSTMT.setString(3, "1000");
+			pSTMT.setInt(4, 2);
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		
+		
+
+		
+		
 	}
 
 }
